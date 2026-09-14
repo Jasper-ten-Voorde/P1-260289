3ae1f9cc8653573db063f705b765a34c96ed4400
Jasper ten Voorde <jsta2805@gmail.com>
2026-09-14 11:41:59 +0200

    opdracht2V0.3

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,31 @@
 function setup() {
   createCanvas(400, 400);
 }
-
+//variabelen met waardens
+let y = 80
+ let x = 100
+ let a = 20
+ let b = 10
 function draw() {
   background(220);
+ //text die de waarde x neer zet
+  text(x, 20, 20);
+ text('Hello world', 20, 60);
+ //variabel een text van de som 20 + 10
+ let optellen = a + b
+ text ('Optellen: ' + optellen, 20, y);
+ noLoop(y = y + 20)
+ //variabel en text van de som 20 - 10
+ let aftreken = a - b
+ text('Aftreken: ' + aftreken, 20, y);
+ noLoop(y = y + 20)
+//variebel en text van de som 20 * 10
+ let vermenigvuldigen = a * b
+ text('vermenigvuldigen: '+ vermenigvuldigen, 20, y);
+ noLoop(y = y + 20)
+ //variabel en text van de som 20 / 10
+ let delen = a / b
+ text('delen: ' + delen, 20, y);
+ 
+
 }
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,21 +1,31 @@
 function setup() {
   createCanvas(800, 600);
+  background(220);
 }
 
 let Xposition1 = 200
 let Xposition2 = 0
+let Yposition1 = 200
+let Yposition2 = 200
 
+let carsize = 20
+let distance = carsize + 5
 function draw() {
   background(220);
- let car1 = rect(0+Xposition1, 200, 20, 20);
-  let car2 = rect(0+Xposition2, 200, 20, 20);
+ let car1 = rect(0+Xposition1, 0+Yposition1, carsize, carsize);
+  let car2 = rect(0+Xposition2, 0+Yposition2, carsize, carsize);
   
  
   
   Xposition1 += 2
   
 
-   if (Xposition2 >= Xposition1 - 25) {
+   if (Xposition2 > Xposition1 && Yposition2 === Yposition1) {
+
+    Xposition2 += 2
+    Yposition2 += 30
+   }
+   else if (Xposition2 >= Xposition1 - distance) {
 
     Xposition2 += 1
    }
@@ -25,4 +35,54 @@ function draw() {
    }
   
   
+   if(Xposition2 > 825){
+
+     Xposition2 = 0
+   }
+   if (Xposition1 > 890) {
+    Xposition1 = 0
+   }
 }
+
+
+
+
+
+
+
+
+
+// let X1 = 200
+// let X2 = 0
+// let Y2 = 200
+// let X3 = 0
+// let Y3 = 175
+
+
+// function draw() {
+  
+//   rect(0+X1, 200, 20, 20);
+//   rect(0+X2, 0+Y2, 20, 20);
+//   rect(0+X3, 0+Y3, 20, 20);
+  
+ 
+//  if (X2 >= X1 - 25) {
+
+//     X2 += 1
+//     X1 += 2
+//    }
+//    else {
+//     X2 += 3
+//     X1 += 2
+//    }
+
+// if (X1 = 825) {
+//   X1 = 200
+// }
+// if (X2 = 825) {
+//   X2 = 0
+// }
+// }
+  
+  
+
